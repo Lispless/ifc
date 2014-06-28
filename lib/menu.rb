b801cf0eeae3d7800a5061644b8ca9c0cc2314f0
K <celfrost@hotmail.com>
2014-06-28 14:57:04 -0400

    Added arrow to menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -15,14 +15,19 @@ class Menu
 	def button_down(id)
 		if id == Gosu::KbUp
 			@select -= 1
-				if @select < 1
-					@select = 2
-				end
+			if @select < 1
+				@select = 2
+			end
 		elsif id == Gosu::KbDown
 			@select += 1
-				if @select > 2
-					@select = 1
-				end
+			if @select > 2
+				@select = 1
+			end
+		elsif id == Gosu::KbReturn
+			if @select == 1
+				@level_1 = true
+			elsif @select == 2
+			end
 		end
 	end
 
@@ -31,5 +36,10 @@ class Menu
 		@font.draw("Incarnae Flare Crepusculem", 8, 10, 1, 1, 1, 0xff0000ff)
 		@font.draw("Play", 180, 450, 1, 1, 1, 0xff0000ff)
 		@font.draw("Options", 180, 500, 1, 1, 1, 0xff0000ff)
+		if @select == 1
+			@menu_arrow.draw(100, 450, 1)
+		elsif @select == 2
+			@menu_arrow.draw(100, 500, 1)
+		end
 	end
 end
